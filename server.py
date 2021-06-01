0323ef0b3bdf0b035f1c50cb98cd7014dac7785e
watchbutdonotlearn <71526591+watchbutdonotlearn@users.noreply.github.com>
2021-06-01 14:55:40 -0500

    Shorten code

--- server.py
+++ server.py
@@ -13,45 +13,23 @@ while True:
     conn, addr = s.accept()     # Establish connection with client.
     print('Got connection from', addr)
     data = conn.recv(1024)
-    # print('Server received', repr(data))
-    serverdo = 2
-    # if data == "GET".encode():
-        # print('Client sent message: GET')
-        # serverdo = 1
-    # else:
-        # print('Client sent message: SEND')
-        # serverdo = 2
     
-    if serverdo == 1:
-        filename='mytext.txt'
-        f = open(filename,'rb')
-        l = f.read(1024)
-        while (l):
-            conn.send(l)
-            # print('Sent '+ repr(l))
-            l = f.read(1024)
+    print('Receiving file')
+    open('received_file', 'w').close()
+    with open('received_file', 'wb') as f:
+        print('file opened')
+        while (data):
+            print('receiving data...')
+            f.write(data)
+            data = conn.recv(1024)
         f.close()
-        print('Done sending')
-    elif serverdo == 2:
-        print('Receiving file')
-        open('received_file', 'w').close()
-        with open('received_file', 'wb') as f:
-            print('file opened')
-            while (data):
-                print('receiving data...')
-                f.write(data)
-                data = conn.recv(1024)
-            f.close()
-        print('copying file to clipboard...')
-        g = open('received_file', 'rb')
-        copytoclip = g.read().decode('UTF-8', 'ignore')
-        print('appending to clipboard:' + copytoclip)
-        try:
-            pyperclip.copy(copytoclip)
-        except:
-            print('no clipboard capibility found')
+    print('copying file to clipboard...')
+    g = open('received_file', 'rb')
+    copytoclip = g.read().decode('UTF-8', 'ignore')
+    print('appending to clipboard:' + copytoclip)
+    try:
+        pyperclip.copy(copytoclip)
+    except:
+        print('no clipboard capibility found')
     
-
-    # message = 'Thank you for connecting'
-    # conn.send(message.encode())
     conn.close()
